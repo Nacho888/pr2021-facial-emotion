b6bcc494e9e8df80c075fc4ca603f136279b1069
Ignacio Montes Álvarez <nachonava98@gmail.com>
2022-01-21 12:49:06 +0100

    Data augmentation, early stopping delta and second dataset addition

--- base.py
+++ base.py
@@ -20,10 +20,34 @@ def load_data(dataset_name, width=None, height=None):
     for emotion in os.listdir(f"datasets/{dataset_name}"):
         for image_name in os.listdir(f"datasets/{dataset_name}/{emotion}"):
             image = cv2.imread(f"datasets/{dataset_name}/{emotion}/{image_name}", cv2.IMREAD_GRAYSCALE)
-            if width is not None and height is not None:
-                image = cv2.resize(image, (width, height))
-            data.append(image)
-            labels.append(emotion)
+
+            if dataset_name == "jaffe":
+                image = image[80:230, 50:200]
+                if width is not None and height is not None:
+                    image = cv2.resize(image, (width, height))
+                img_flip_lr = cv2.flip(image, 1)
+                img_rotate_90_clockwise = cv2.rotate(image, cv2.ROTATE_90_CLOCKWISE)
+                img_rotate_90_counter_clockwise = cv2.rotate(image, cv2.ROTATE_90_COUNTERCLOCKWISE)
+                img_rotate_180 = cv2.rotate(image, cv2.ROTATE_180)
+
+                images = [image, img_flip_lr, img_rotate_90_clockwise, img_rotate_90_counter_clockwise, img_rotate_180]
+
+                for img in images:
+                    data.append(img)
+                    labels.append(emotion)
+            else:
+                if width is not None and height is not None:
+                    image = cv2.resize(image, (width, height))
+
+                data.append(image)
+                labels.append(emotion)
+
+    plt.hist(labels, bins=np.arange(7) - 0.5, rwidth=0.8)  # set arange to number of classes + 1
+    plt.title(f"'{dataset_name}' Class distribution (N={len(labels)})")
+    plt.xlabel("Class label")
+    plt.ylabel("Frequency")
+    plt.savefig(f"img/{dataset_name}_class_distribution.png")
+    plt.show()
 
     fig, axs = plt.subplots(3, 3)
     for _i in range(len(axs)):
@@ -76,7 +100,7 @@ def create_model(hp):
                                             default=0.2,
                                             step=0.1)))
 
-    model_architecture.add(Dense(7))
+    model_architecture.add(Dense(6))
     model_architecture.add(Activation("softmax"))
 
     optimizer = hp.Choice("optimizer", values=["adam", "rmsprop", "sgd", "adagrad"], default="adam")
@@ -141,47 +165,78 @@ def plot_loss_acc_from_history(hist, dataset_name):
     plt.show()
 
 
-dataset = "ck-plus"
-X, y = load_data(dataset)
-X = np.expand_dims(X, 3)
-w = X.shape[1]
-h = X.shape[2]
-print(f"Input shape: ({w}x{h})")
-# total_emotions = len(Counter(y).keys())
+# MAIN
+datasets = [{"name": "jaffe", "raw_data": [], "matrices": [], "model": None, "scores": []},
+            {"name": "ck-plus", "raw_data": [], "matrices": [], "model": None, "scores": []}]
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=8)
+for dataset in datasets:
+    X, y = load_data(dataset["name"], 48, 48)
+    X = np.expand_dims(X, 3)
+    w = X.shape[1]
+    h = X.shape[2]
+    print(f"Input shape: ({w}x{h})")
 
-tuner_rs = RandomSearch(
-    create_model,
-    objective="val_sparse_categorical_accuracy",
-    seed=8,
-    max_trials=10,
-    executions_per_trial=2,
-    overwrite=True)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=8)
 
-n_epochs = 15
-with tf.device("/gpu:0"):  # if there is an available GPU, use it
-    print("Starting tuner...")
-    stop_early = EarlyStopping(monitor="val_loss", patience=5)
-    tuner_rs.search(X_train, y_train, epochs=n_epochs, validation_split=0.1, verbose=1, callbacks=[stop_early])
+    tuner_rs = RandomSearch(
+        create_model,
+        objective="val_sparse_categorical_accuracy",
+        seed=8,
+        max_trials=10,
+        executions_per_trial=2,
+        overwrite=True)
 
-# Retrain best model on full dataset with best hyperparameters
-print("Retraining best model...")
-best_hps = tuner_rs.get_best_hyperparameters(num_trials=1)[0]
-hypermodel = tuner_rs.hypermodel.build(best_hps)
-history = hypermodel.fit(X_train, y_train, epochs=50, validation_split=0.1, callbacks=[stop_early])
-print("Best model architecture")
-print(hypermodel.summary())
-print("Best model hyperparams")
-tuner_rs.results_summary(1)
+    n_epochs = 15
+    with tf.device("/gpu:0"):  # if there is an available GPU, use it
+        print("Starting tuner...")
+        stop_early = EarlyStopping(monitor="val_loss", min_delta=0.075, patience=3)
+        tuner_rs.search(X_train, y_train, epochs=n_epochs, validation_split=0.1, verbose=1, callbacks=[stop_early])
 
-# Evaluate and plot confusion matrix
-eval_result = hypermodel.evaluate(X_test, y_test)
-print("[test loss, test accuracy]:", eval_result)
-disp = ConfusionMatrixDisplay(confusion_matrix=confusion_matrix(y_test, hypermodel.predict_classes(X_test)))
+    # Retrain best model on full dataset with best hyperparameters
+    print("Retraining best model...")
+    best_hps = tuner_rs.get_best_hyperparameters(num_trials=1)[0]
+    hypermodel = tuner_rs.hypermodel.build(best_hps)
+    history = hypermodel.fit(X_train, y_train, epochs=50, validation_split=0.1, callbacks=[stop_early])
+    print("Best model architecture")
+    print(hypermodel.summary())
+    print("Best model hyperparams")
+    tuner_rs.results_summary(1)
+
+    # Evaluate and plot confusion matrix
+    eval_result = hypermodel.evaluate(X_test, y_test, verbose=0)
+    print(f"{dataset['name']} dataset [test loss, test accuracy]:", eval_result)
+    disp = ConfusionMatrixDisplay(confusion_matrix=confusion_matrix(y_test, hypermodel.predict_classes(X_test)))
+    disp.plot(values_format="d")
+    plt.savefig(f"img/{dataset['name']}_confusion_matrix.png")
+    plt.show()
+
+    # Plot loss vs accuracy
+    plot_loss_acc_from_history(history, dataset['name'])
+
+    dataset["raw_data"] = [X, y]
+    dataset["matrices"] = [X_train, X_test, y_train, y_test]
+    dataset["model"] = hypermodel
+    dataset["scores"] = [hypermodel.evaluate(X_train, y_train, verbose=0), eval_result]
+
+eval_result = datasets[0]["model"].evaluate(datasets[1]["matrices"][1], datasets[1]["matrices"][3], verbose=0)
+print("Caucasian model using Asian data [test loss, test accuracy]:", eval_result)
+disp = ConfusionMatrixDisplay(confusion_matrix=confusion_matrix(datasets[1]["matrices"][3],
+                                                                datasets[0]["model"].predict_classes(datasets[1]["matrices"][1])))
 disp.plot(values_format="d")
-plt.savefig(f"img/{dataset}_confusion_matrix.png")
+plt.savefig(f"img/{datasets[0]['name']}_asian_confusion_matrix.png")
 plt.show()
 
-# Plot loss vs accuracy
-plot_loss_acc_from_history(history, dataset)
+eval_result = datasets[1]["model"].evaluate(datasets[0]["matrices"][1], datasets[0]["matrices"][3], verbose=0)
+print("Asian model using Caucasian data [test loss, test accuracy]:", eval_result)
+disp = ConfusionMatrixDisplay(confusion_matrix=confusion_matrix(datasets[0]["matrices"][3],
+                                                                datasets[1]["model"].predict_classes(datasets[0]["matrices"][1])))
+disp.plot(values_format="d")
+plt.savefig(f"img/{datasets[1]['name']}_caucasian_confusion_matrix.png")
+plt.show()
+
+# Summary of results
+for dataset in datasets:
+    print(f"{dataset['name']} dataset:")
+    print(f"\tTraining score: {dataset['scores'][0][1]}")
+    print(f"\tTest score: {dataset['scores'][1][1]}")
+
